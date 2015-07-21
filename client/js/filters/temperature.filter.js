46082258f8c656c7b410961197277396cd00cc1a
Tomas Holub <tomas.holub@unic.com>
2015-07-21 12:04:32 +0200

    cleaning the code

--- client/js/filters/temperature.filter.js
+++ client/js/filters/temperature.filter.js
@@ -1,8 +1,10 @@
 angular.module('app.filters').filter('temperature', function(){
-    return function(items, value) {
-        if (items && value) {
+    return function(items, highest, lowest) {
+        var highest = highest || 100;
+        var lowest  = lowest  || -100;
+        if (items) {
             return items.filter(function(item){
-                return item.high >= value;
+                return (item.high <= highest) && (item.low > lowest) ;
             });
         }
         return items;
