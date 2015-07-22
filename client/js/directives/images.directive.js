f0ec84d3c8bc8b5cde18c5aebfe497209bbb10a9
Tomas Holub <tomas.holub@unic.com>
2015-07-22 12:31:33 +0200

    images directive template change, resource param update

--- client/js/directives/images.directive.js
+++ client/js/directives/images.directive.js
@@ -1,11 +1,11 @@
 angular.module('app.directives').directive('images', function() {
     return {
         restrict: 'E',
-        template: '<p ng-repeat="item in images track by item.imageId">' +
+        template: '<ul  class="small-block-grid-1 large-block-grid-2"><li ng-repeat="item in images track by item.imageId">' +
                         '<a ng-href="{{ item.url }}" title="{{ item.contentNoFormatting }}">' + 
                             '<img ng-src="{{ item.tbUrl }}">' +
                         '</a>' +
-                  '</p>',
+                  '</li></ul>',
         
         link: function(scope, el) {
             scope.$watch(function() {
--- client/js/services/images.factory.js
+++ client/js/services/images.factory.js
@@ -1,8 +1,8 @@
 angular.module('app.services').factory('ImagesFactory', function($resource){
-    return $resource('//ajax.googleapis.com/ajax/services/search/images',{},{ 
+    return $resource('//ajax.googleapis.com/ajax/services/search/images',{},{
         load: { 
             method: 'JSONP', 
-            params: { v: '1.0', callback: 'JSON_CALLBACK' } 
+            params: { v: '1.0', callback: 'JSON_CALLBACK',imgsz:'large' }
         } 
     });
 });
