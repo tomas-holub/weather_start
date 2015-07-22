77dbd6cc0a4ae30059b013a4de2f93a42d687731
Tomas Holub <tomas.holub@unic.com>
2015-07-22 12:44:15 +0200

    image size

--- client/js/services/images.factory.js
+++ client/js/services/images.factory.js
@@ -2,7 +2,7 @@ angular.module('app.services').factory('ImagesFactory', function($resource){
     return $resource('//ajax.googleapis.com/ajax/services/search/images',{},{
         load: { 
             method: 'JSONP', 
-            params: { v: '1.0', callback: 'JSON_CALLBACK',imgsz:'large' }
+            params: { v: '1.0', callback: 'JSON_CALLBACK',imgsz:'medium' }
         } 
     });
 });
