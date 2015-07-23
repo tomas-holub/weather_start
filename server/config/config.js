56f31be096f8e7865a7ea999a889b3e08e247ed2
Tomas Holub <tomas.holub@unic.com>
2015-07-23 09:27:39 +0200

    clean

--- server/config/config.js
+++ server/config/config.js
@@ -1,24 +1,13 @@
 var config = {
     development: {
-        dbName: 'tom',
-        dbHost: 'localhost',
-        dbPort: 27017,
         serverPort: 3000,
         serverIp:'127.0.0.1'
     },
     test: {
-        dbName: 'test',
-        dbHost: 'localhost',
-        dbPort: 27017,
         serverPort: 8888,
         serverIp:'127.0.0.1'
     },
     production: {
-        dbUsername: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
-        dbPassword: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
-        dbHost: process.env.OPENSHIFT_MONGODB_DB_HOST,
-        dbPort: process.env.OPENSHIFT_MONGODB_DB_PORT,
-        dbName: process.env.OPENSHIFT_APP_NAME,
         serverPort: process.env.OPENSHIFT_NODEJS_PORT,
         serverIp: process.env.OPENSHIFT_NODEJS_IP
     }
